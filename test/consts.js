55241787635b0eb8eb5b72b2fb1ef3e2082ae146
Salvatore Laisa <moebiusmania@gmail.com>
2018-09-24 12:42:19 +0200

    updated tests

--- test/consts.js
+++ test/consts.js
@@ -6,15 +6,15 @@ test('API base url', t => {
 });
 
 test('PATHS properties & methods', t => {
-  const id = 42;
-  const posts = 'posts?per_page=12&_embed=';
-  const post = `posts/${id}?_embed=`;
-  const page = `pages/${id}?_embed=`;
+  const slug = 'example';
+  const posts = 'posts?per_page=12';
+  const post = `posts?slug=${slug}&_embed=`;
+  const page = `pages?slug=${slug}&_embed=`;
 
   t.is(PATHS.tags, 'tags');
   t.is(PATHS.menu, 'menus/nav');
   t.is(PATHS.pages, 'pages');
   t.true(PATHS.posts().indexOf(posts) > -1);
-  t.true(PATHS.post(id).indexOf(post) > -1);
-  t.true(PATHS.page(id).indexOf(page) > -1);
+  t.true(PATHS.post(slug).indexOf(post) > -1);
+  t.true(PATHS.page(slug).indexOf(page) > -1);
 });
