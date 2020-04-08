22e844472219ba25903a18514fd5ed4c17ababc5
Erik Fazio <erik@MBP-di-Erik.homenet.telecomitalia.it>
2020-04-08 16:53:03 +0200

    Styled cover section for mobile devices

--- src/.vuepress/theme/components/Cover.vue
+++ src/.vuepress/theme/components/Cover.vue
@@ -1,6 +1,12 @@
 <template>
   <section class="cover">
-    <video id="businessVideo" autoplay muted loop>
+    <div class="cover__description">
+      <h3 class="cover__tagline">{{ tagline }}</h3>
+      <h2 class="cover__claim">{{ claim }}</h2>
+      <p class="cover__text"><b>Bitrock</b> is a <b>high-end consulting and system integration company</b>, committed to offering cutting-edge and innovative solutions in <b>Microservices</b>, <b>Reactive Platforms</b>, <b>Data Analytics</b>, <b>DevOps</b> and <b>UX</b>.</p>
+      <a class="btn btn-1" :href="ctaLink">{{ ctaText }}</a>
+    </div>
+    <video class="cover__video" autoplay muted loop>
       <source src="videos/business.mp4" type="video/mp4">
     </video>
   </section>
