a4b9969bd7ef14ce5e3748e6c4a73dc5f3cd0a7b
Erik Fazio <erik@MBP-di-Erik.homenet.telecomitalia.it>
2020-04-09 11:28:41 +0200

    Added new font for home

--- src/.vuepress/theme/components/Cover.vue
+++ src/.vuepress/theme/components/Cover.vue
@@ -3,30 +3,17 @@
     <div class="cover__description">
       <h3 class="cover__tagline">{{ tagline }}</h3>
       <h2 class="cover__claim">{{ claim }}</h2>
-      <p class="cover__text">Bitrock is a high-end consulting and system integration company,<br/>
-      committed to offering cutting-edge<br/>
-      and innovative solutions in Microservices,<br/>
-      Reactive Platforms, Data Analytics, DevOps and UX.</p>
+      <p class="cover__text">
+        Bitrock is a high-end technology<br/>
+        and consulting firm, committed to offering cutting-edge<br/>
+        and innovative solutions in DevOps,<br/>
+        Custom Software Engineering and Data&Analytics</p>
       <a class="cover__button" :href="ctaLink">{{ ctaText }}</a>
     </div>
     <video class="cover__video" autoplay muted loop>
       <source src="videos/business.mp4" type="video/mp4">
     </video>
   </section>
-  <!-- 
-    <section class="cover">
-    <figure style="background-image: url(/img/main_bg.jpg)">
-      <figcaption class="wrapper">
-        <div class="claim">
-          <h3>{{ tagline }}</h3>
-          <h2>{{ claim }}</h2>
-          <p><b>Bitrock</b> is a <b>high-end consulting and system integration company</b>, committed to offering cutting-edge and innovative solutions in <b>Microservices</b>, <b>Reactive Platforms</b>, <b>Data Analytics</b>, <b>DevOps</b> and <b>UX</b>.</p>
-        </div>
-        <a class="btn btn-1" :href="ctaLink">{{ ctaText }}</a>
-      </figcaption>
-    </figure>
-  </section>
-  -->
 </template>
 
 <script>
