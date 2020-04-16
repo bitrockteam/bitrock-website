2a52ef4ac5da6abe21469523f91041f0903ba5d3
erikfazio <fazioerik@gmail.com>
2020-04-16 15:41:12 +0200

    Feature/updates website (#50)
    * Added video for home page
    
    * Added responsive video on home page
    
    * Fixed responsive video for all devices
    
    * Styled cover section for mobile devices
    
    * Changed claim and tagline
    
    * Fixed various problems with responsive on home page
    
    * Added new font for home
    
    * Added mode colors for logo and text
    
    * Fix on svg logo
    
    * Fixed responsive on various devices for home page cover
    
    * Edited text on "About" page
    
    * Configured css module for "About" page
    
    * Added gray background color for "About" page
    
    * Configured scoping of css for "About" page
    
    * Edited footer component
    
    * Fix on footer padding
    
    * Added introduction component
    
    * Added mission component
    
    * Added offering component
    
    * Added images for partners
    
    * Added technology enablers component
    
    * Added binoculars image
    
    * Added offering component
    
    * Fixed spacing between blocks
    
    * Fixed issues with headings and spacings in "About" page
    
    * Edited text on "Academy" page
    
    * Fixed color of menu links
    
    * fixed header hamburger color
    
    * Fixed hover color for menu links
    
    * Fixed errors
    
    * Changed font for blockquote
    
    * Fixed menu item colors
    
    * Fixed light and dark logo on header with responsive mode
    
    * Fix on white links on header
    
    Co-authored-by: Erik Fazio <erik@MBP-di-Erik.homenet.telecomitalia.it>

--- src/.vuepress/theme/components/Cover.vue
+++ src/.vuepress/theme/components/Cover.vue
@@ -1,15 +1,18 @@
 <template>
   <section class="cover">
-    <figure style="background-image: url(/img/main_bg.jpg)">
-      <figcaption class="wrapper">
-        <div class="claim">
-          <h3>{{ tagline }}</h3>
-          <h2>{{ claim }}</h2>
-          <p><b>Bitrock</b> is a <b>high-end consulting and system integration company</b>, committed to offering cutting-edge and innovative solutions in <b>Microservices</b>, <b>Reactive Platforms</b>, <b>Data Analytics</b>, <b>DevOps</b> and <b>UX</b>.</p>
-        </div>
-        <a class="btn btn-1" :href="ctaLink">{{ ctaText }}</a>
-      </figcaption>
-    </figure>
+    <div class="cover__description">
+      <h3 class="cover__tagline">{{ tagline }}</h3>
+      <h2 class="cover__claim">{{ claim }}</h2>
+      <p class="cover__text">
+        Bitrock is a high-end technology<br/>
+        and consulting firm, committed to offering cutting-edge<br/>
+        and innovative solutions in DevOps,<br/>
+        Custom Software Engineering and Data&amp;Analytics.</p>
+      <a class="cover__button" :href="ctaLink">{{ ctaText }}</a>
+    </div>
+    <video class="cover__video" autoplay muted loop>
+      <source src="videos/business.mp4" type="video/mp4">
+    </video>
   </section>
 </template>
 
