679ced231302eed805c2364cc0f103cd2454b64b
Erik Fazio <fazioerik@gmail.com>
2020-06-29 16:38:24 +0200

    Edited bitrock contact info

---
+++ src/.vuepress/theme/components/Post.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card">
+        <router-link :to="post.path" :aria-label="post.title">
+        <header style="z-index: 97;position: relative;">
+            <figure>
+            <img :src="post.frontmatter.image" :alt="post.title" />
+            </figure>
+        </header>
+        <article>
+            <h4>{{ post.title }}</h4>
+        </article>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['post']
+}
+</script>
+
+
+<style lang="scss">
+@import './../styles/main.scss';
+</style>
