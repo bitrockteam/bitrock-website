ba65110d47c26954b73041bb3e9453b50eca9f5c
Yi Zhang <16437654+zhango90@users.noreply.github.com>
2020-10-29 15:50:18 +0100

    modifid vuepress configuration and added script (#75)

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,4 +1,4 @@
-const pkg = require('./../../package.json');
+const pkg = require("./../../package.json");
 
 module.exports = {
   title: pkg.displayName,
@@ -6,37 +6,43 @@ module.exports = {
 
   themeConfig: {
     nav: [
-      { text: 'About', link: '/discover' },
-      { text: 'Academy', link: '/academy' },
-      { 
-        text: 'Join Us', 
-        link: 'https://www.linkedin.com/company/bitrock-srl/jobs/',
+      { text: "About", link: "/discover" },
+      { text: "Academy", link: "/academy" },
+      {
+        text: "Join Us",
+        link: "https://www.linkedin.com/company/bitrock-srl/jobs/",
         external: true
       },
-      { text: 'Blog', link: '/blog' },
+      { text: "Blog", link: "/blog" }
     ]
   },
 
   head: [
-    ['link', { rel: 'icon', href: '/logotype.png' }],
-    ['link', { rel: 'manifest', href: '/app.webmanifest' }],
-    ['meta', { name: 'theme-color', content: pkg.config.themeColor }],
-    ['meta', { name: 'author', content: pkg.displayName }],
-    ['meta', { name: 'title', property: 'og:title', content: pkg.displayName }],
-    ['meta', { name: 'og:description', content: pkg.description }],
-    ['meta', { name: 'image', property: 'og:image', content: '/social.png' }],
+    ["link", { rel: "icon", href: "/logotype.png" }],
+    ["link", { rel: "manifest", href: "/app.webmanifest" }],
+    ["meta", { name: "theme-color", content: pkg.config.themeColor }],
+    ["meta", { name: "author", content: pkg.displayName }],
+    ["meta", { name: "title", property: "og:title", content: pkg.displayName }],
+    ["meta", { name: "og:description", content: pkg.description }],
+    ["meta", { name: "image", property: "og:image", content: "/social.png" }]
   ],
 
   plugins: [
-    '@vuepress/nprogress',
-    ['@vuepress/google-analytics', {
-      'ga': 'UA-127344693-1'
-    }],
-    '@vuepress/back-to-top',
-    ['@vuepress/pwa', {
-      'serviceWorker': true,
-      // 'popupComponent': 'UpdatePopup',
-      'updatePopup': true,
-    }]
+    "@vuepress/nprogress",
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-127344693-1"
+      }
+    ],
+    "@vuepress/back-to-top",
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: false,
+        // 'popupComponent': 'UpdatePopup',
+        updatePopup: true
+      }
+    ]
   ]
-}
+};
